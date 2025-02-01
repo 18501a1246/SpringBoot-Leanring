9577f6f0b174bb250235f3ab87a68e152a0473ce
mohan <18501a1246@gmail.com>
2025-02-01 16:19:54 +0530

    Spring Data JPA DEMO

---
+++ spring-data-jan18/src/main/java/com/kmk/SpringDataJan18Application.java
@@ -0,0 +1,13 @@
+package com.kmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJan18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJan18Application.class, args);
+	}
+
+}
